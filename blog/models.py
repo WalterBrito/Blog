83ec0465e6d97537627309a2d80947d2452ed3eb
Fedora <waltter.britto@gmail.com>
2016-06-30 17:58:23 -0300

    Simple blog made in django

--- blog/models.py
+++ blog/models.py
@@ -4,30 +4,30 @@ from django.contrib.auth.models import User
 
 
 class PublishedManager(models.manager):
-	def get_queryset(self):
-		return super(PublishedManager, self).get_queryset().filter(status='published') 
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
-	STATUS_CHOICES = (
-		('draft', 'Draft'),
-		('published', 'Published')
-	)
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
 
-	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250, unique_for_date='publish')
-	author = models.ForeignKey(User, related_name='blog_posts')
-	body = models.TextField()
-	publish = models.DateTimeField(default=timezone.now)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-	objects = models.Manager()  # The default manager
-	published = PublishedManager()  # Our custom manger 
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Our custom manger
 
-	class Meta:
-		ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
-	def __str__(self):
-		return self.title
-
-
+    def __str__(self):
+        return self.title
