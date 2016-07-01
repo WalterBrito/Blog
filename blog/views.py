12668a4845b96f2cdf3661d1ed63f96734adcb29
Fedora <waltter.britto@gmail.com>
2016-07-01 11:24:34 -0300

    Simple blog made in django

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
     posts = Post.published.all()
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
-    	posts = paginator.page(page)
+        posts = paginator.page(page)
     except PageNotAnInteger:
-    	# If page is not an integer deliver the first page
-    	posts = paginator.page(1)
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
     except EmptyPage:
-    	# If page is out of range deliver last page of results
-    	posts = paginator.page(paginator.num_pages)
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
